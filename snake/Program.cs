f6ba977db18c6e3840fcca036c18aa0fa834c49d
maybefriend <alex.post-x@yandex.ru>
2016-11-03 13:52:41 +0300

    func

--- snake/Program.cs
+++ snake/Program.cs
@@ -9,8 +9,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hallo");
+            int x1 = 1, y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 4, y2 = 5;
+            char sym2 = 'm';
+
+            Draw(x2, y2, sym2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)//функция
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
